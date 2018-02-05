9c938aa90182840d9ecf6bad6451c5a680b0f7a3
PatrickConfer <pconfer@gmail.com>
2018-02-05 16:50:04 -0500

    working KataStringCalculator

--- m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
+++ m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
@@ -1,5 +1,65 @@
 package com.techelevator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 public class KataStringCalculator {
 
+	
+	public int add(String numbers) {
+		
+		int result = 0;
+		
+		if (numbers == "") {
+			result = 0;
+		}
+		
+		else {
+			
+			List<String> list = new ArrayList<String>();
+			
+			list = Arrays.asList(numbers.split(","));
+			Stack<String> theStack = new Stack<String>();
+			int[] nums = new int[list.size()];
+			
+			for (String s : list) {
+				theStack.push(s);
+			}
+			
+			for (int i = 0; i < list.size(); i++) {
+				nums[i]= Integer.parseInt(theStack.pop());
+				result += nums[i];
+				System.out.println(nums[i]);
+			}
+
+		}
+		return result;
+	}
 }
+		
+	
+
+//nums = list.toArray();
+//for(int i = 0; i < list.size(); i++) {
+//
+//}
+
+
+//String[] splitNums = new String[10];
+//int[] nums = new int[10];
+//splitNums = numbers.split(",");
+
+//int add = 0;
+//for (String i: list) {
+//
+//nums[i] = Integer.parseInt(list[i]);
+//add += nums[i];
+//result =  nums[0] + nums[1];
+//
+//}
+		
+	
+	
+
--- m1-w4d1-tdd-exercises-pair/src/test/java/com/techelevator/KataStringCalculatorTest.java
+++ m1-w4d1-tdd-exercises-pair/src/test/java/com/techelevator/KataStringCalculatorTest.java
@@ -4,5 +4,52 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataStringCalculatorTest {
+	
+	public KataStringCalculator calc;
+	@Before
+	public void setup() {
+		calc = new KataStringCalculator();
+	}
+	
+	/*
+	 * "" returns 0. check.
+	 * "1" returns 1. check.
+	 * "1,2" returns sum 3. 
+	 * convert string to integer.
+	 * 
+	 * Passing multiple numbers to method returns their sum.
+	 * method handles \n delimiter.
+	 * method handles any delimiter (too big?)
+	 */
+	
+	@Test
+	public void empty_string_returns_0() {
+		
+		Assert.assertEquals(0, calc.add(""));
+		
+	}
+	
+	@Test
+	public void convert_string_result_to_integer() {
+		
+		Assert.assertEquals(0, calc.add(""));
+		Assert.assertEquals(-2, calc.add("-2"));
+		Assert.assertEquals(20, calc.add("20"));
+	}
+	@Test
+	public void addition_of_two_numbers_in_String() {
+		
+		Assert.assertEquals("Should return sum", 3, calc.add("1,2"));
+		Assert.assertEquals("Should return sum", -5, calc.add("2,-7"));
+		Assert.assertEquals("Should return sum", 50, calc.add("20,30"));
+	}
+	
+	@Test
+	public void method_adding_an_unknown_amount_of_numbers() {
+		Assert.assertEquals("Should return sum", 23, calc.add("9,8,6"));
+	}
+	
+	
+	
 
 }
