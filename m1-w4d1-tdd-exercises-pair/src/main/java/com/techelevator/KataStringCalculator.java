8571250daa5e661f46f2e3cd29b5ed2a4d0c788a
PatrickConfer <pconfer@gmail.com>
2018-02-06 14:29:42 -0500

    KataStringCalculator

--- m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
+++ m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
@@ -11,79 +11,58 @@ public class KataStringCalculator {
 	public int add(String numbers) {
 		
 		int result = 0;
-		
-		if (numbers == "") {
-			result = 0;
-		}
-		
-		else {
+		String help = "";
+		Stack<String> theStack = new Stack<String>();
+		List<String> list = new ArrayList<String>();
+	
+		if (numbers != "") {
 			
 			String text = numbers.replaceAll("[^a-zA-Z0-9-]", ",");
-			System.out.println(text);
-			String help = "";
-			String done = "";
+			
+			//remove extra commas, put remainder in new string
 			for(int i = 0; i < text.length() - 1; i++) {
-				if (text.charAt(i) != ',' || (text.charAt(i) == ',' && text.charAt(i + 1) != ',' )) {
+				if (text.charAt(i) != ',' || ((text.charAt(i) == ',') && (text.charAt(i + 1) != ','))) {
 					help+=text.charAt(i);
-
-					
 				}
-
 			}
+			//add back lost character
 			if (text.charAt(text.length() -1) != ',') {
 				help+= text.charAt(text.length() -1);
 			}
+			//remove leading comma
 			if (text.charAt(0) == ',') {
-				done = help.substring(1);
+				help = help.substring(1);
 			}
-			System.out.println(help);
-			System.out.println(done);;
-			
-			List<String> list = new ArrayList<String>();
-			
-			list = Arrays.asList(done.split(","));
-			Stack<String> theStack = new Stack<String>();
-			for(String s : list) {
-				System.out.println(s);
+			//remove trailing comma
+			if (help.charAt((help.length() -1 )) == ',') {
+				help = help.substring(0, help.length() - 1);
 			}
 			
-			int[] nums = new int[list.size()];
-			
+			//split the string
+			list = Arrays.asList(help.split(","));
+
+			//push from listArray to Stack
 			for (String s : list) {
 				theStack.push(s);
 			}
-			
-			for (int i = 0; i < list.size(); i++) {
-				nums[i]= Integer.parseInt(theStack.pop());
-				result += nums[i];
-//				System.out.println(nums[i]);
+			//pop, parse, add
+
+			while (!theStack.empty()) {
+				result+= Integer.parseInt(theStack.pop());
+
 			}
 
-		}
-		return result;
+		
+	
+		
 	}
+		return result;
+}
 }
 		
 	
 
-//nums = list.toArray();
-//for(int i = 0; i < list.size(); i++) {
-//
-//}
 
-
-//String[] splitNums = new String[10];
-//int[] nums = new int[10];
-//splitNums = numbers.split(",");
-
-//int add = 0;
-//for (String i: list) {
-//
-//nums[i] = Integer.parseInt(list[i]);
-//add += nums[i];
-//result =  nums[0] + nums[1];
-//
-//}
 		
 	
 	
