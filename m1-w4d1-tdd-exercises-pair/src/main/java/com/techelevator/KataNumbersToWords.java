571a525e190f50c8ec9bb287d37f8d0cf13089b6
anastasia <anastasia.prokofyeva@gmail.com>
2018-02-06 19:15:03 -0500

    stopping point for the night

--- m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataNumbersToWords.java
+++ m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataNumbersToWords.java
@@ -1,5 +1,157 @@
 package com.techelevator;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ *     ***********Still working on final solution for conversion to numbers*************
+ * 
+ */
+
+
+
+
 public class KataNumbersToWords {
+	
+	Map<Integer, String> oneDigit = new HashMap<Integer, String>();
+	Map<Integer, String> twoDigit = new HashMap<Integer, String>();
+	
+	public String converterToWords(int number) {
+		
+		String tempNum = Integer.toString(number);
+		String result = "";
+		
+		
+		
+		
+		if (number >= 20) {
+						
+			String reverse = "";
+			for(int i = tempNum.length()-1; i > - 1; i--) {
+				reverse += Character.toString(tempNum.charAt(i));
+			}
+			//ten-thousands
+			if(reverse.length() == 5) {
+				if(number % 10000 == 0) {
+					if (number == 10000) {
+						return "ten thousand";
+					}
+					return (twoDigit.get(number/10000) + " thousand");
+				}
+				if((reverse.charAt(4) == '1') && (reverse.charAt(5) !=0)) {
+					reverse = reverse.substring(4);
+					int teens = Integer.parseInt(reverse);
+					result += oneDigit.get(teens);
+					return result;
+				}
+				int thouDigit = Character.getNumericValue(reverse.charAt(3));
+				result += (oneDigit.get(thouDigit) + " thousand and ");
+				reverse = reverse.substring(0, reverse.length() -1);
+			}
+			//thousands
+			if(reverse.length() == 4) {
+				if(number % 1000 == 0) {
+					return (oneDigit.get(number/1000) + " thousand");
+				}
+				int thouDigit = Character.getNumericValue(reverse.charAt(3));
+				result += (oneDigit.get(thouDigit) + " thousand and ");
+				reverse = reverse.substring(0, reverse.length() -1);
+			}
+			
+			//hundreds
+			if(reverse.length() == 3) {
+				if(number % 100 == 0) {
+					return (oneDigit.get(number/100) + " hundred");
+				}
+
+				int hundDigit = Character.getNumericValue(reverse.charAt(2));
+				if (hundDigit == 0) {
+					result += "";
+				}
+				else {
+					result += (oneDigit.get(hundDigit) + " hundred and ");
+				}
+			}
+	
+			
+			//tens under 100
+			
+			if(number % 10 == 0 && number < 100) {
+				return twoDigit.get(number/10);
+			}
+			
+			
+			if((reverse.charAt(0) == '1') && (reverse.charAt(1) !=0)) {
+				reverse = reverse.substring(0, reverse.length() - 1);
+				String reverseBack = "";
+				for(int i = reverse.length()-1; i > - 1; i--) {
+					reverseBack += Character.toString(reverse.charAt(i));
+				}
+				
+				
+				int teens = Integer.parseInt(reverseBack);
+				result += oneDigit.get(teens);
+				return result;
+			}
+			//two-digit non-10s
+			int tensDigit = Character.getNumericValue(reverse.charAt(1));
+			
+			if (tensDigit == 0) {
+				result += "";
+			}
+			else {
+				result += twoDigit.get(tensDigit);
+				result += "-";
+			}
+			
+
+			//last digit
+			int lastDigit = Character.getNumericValue(reverse.charAt(0));
+			result += oneDigit.get(lastDigit);
+			return result;
+			
+		} 
+		
+
+			
+		else {
+		
+		return oneDigit.get(number);
+		}
+	}
+	
+	public KataNumbersToWords() {
+		oneDigit.put(0, "zero");
+		oneDigit.put(1, "one");
+		oneDigit.put(2, "two");
+		oneDigit.put(3, "three");
+		oneDigit.put(4, "four");
+		oneDigit.put(5, "five");
+		oneDigit.put(6, "six");
+		oneDigit.put(7, "seven");
+		oneDigit.put(8, "eight");
+		oneDigit.put(9, "nine");
+		oneDigit.put(10, "ten");
+		oneDigit.put(11, "eleven");
+		oneDigit.put(12, "twelve");
+		oneDigit.put(13, "thirteen");
+		oneDigit.put(14, "fourteen");
+		oneDigit.put(15, "fifteen");
+		oneDigit.put(16, "sixteen");
+		oneDigit.put(17, "seventeen");
+		oneDigit.put(18, "eighteen");
+		oneDigit.put(19, "nineteen");
+		twoDigit.put(2, "twenty");
+		twoDigit.put(3, "thirty");
+		twoDigit.put(4, "fourty");
+		twoDigit.put(5, "fifty");
+		twoDigit.put(6, "sixty");
+		twoDigit.put(7, "seventy");
+		twoDigit.put(8, "eighty");
+		twoDigit.put(9, "ninety");
+
+		
+	}
 
 }
+
--- m1-w4d1-tdd-exercises-pair/src/test/java/com/techelevator/KataNumbersToWordsTest.java
+++ m1-w4d1-tdd-exercises-pair/src/test/java/com/techelevator/KataNumbersToWordsTest.java
@@ -4,5 +4,64 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataNumbersToWordsTest {
+	
+	/*
+	 * 1 digit numbers (zero, three, seven)
+	 * 2 digit numbers (ten, fourteen, twenty-six)
+	 * 3 digit numbers (two hundred and nine, three hundred, four hundred and ninety-eight)
+	 * 4 digit numbers (three thousand and four, five thousand and twenty-six, seven thousand and one hundred and eleven)
+	 * 5 digit numbers (forty thousand, eighty-seven thousand and six hundred and fifty-four)
+	 * 6 digit numbers (five hundred thousand, eight hundred and three thousand and three hundred and eight, nine hundred and ninety-nine thousand and nine-hundred and ninety-nine)
+	 */
+	
+	private KataNumbersToWords ntn;
+	
+	@Before
+	public void setup() {
+		ntn = new KataNumbersToWords();
+	}
+	
+	@Test
+	public void return_one_digit_number_in_words() {
+		Assert.assertEquals("zero", ntn.converterToWords(0));
+		Assert.assertEquals("three", ntn.converterToWords(3));
+		Assert.assertEquals("seven", ntn.converterToWords(7));
+	}
+	@Test
+	public void return_two_digit_10s_in_words() {
+		Assert.assertEquals("twenty", ntn.converterToWords(20));
+		Assert.assertEquals("fourty", ntn.converterToWords(40));
+		Assert.assertEquals("eighty", ntn.converterToWords(80));
+	}
+	
+	@Test
+	public void return_two_digit_number_in_words() {
+		Assert.assertEquals("ten", ntn.converterToWords(10));
+		Assert.assertEquals("fourteen", ntn.converterToWords(14));
+		Assert.assertEquals("twenty-six", ntn.converterToWords(26));
+		Assert.assertEquals("seventy-three", ntn.converterToWords(73));
+	}
+	
+	@Test
+	public void return_three_digit_number_in_words() {
+		Assert.assertEquals("two hundred and nine", ntn.converterToWords(209));
+		Assert.assertEquals("three hundred", ntn.converterToWords(300));
+		Assert.assertEquals("four hundred and ninety-eight", ntn.converterToWords(498));
+		Assert.assertEquals("four hundred and twelve", ntn.converterToWords(412));
+	}
+	
+	@Test
+	public void return_four_digit_number_in_words() {
+		Assert.assertEquals("three thousand and four", ntn.converterToWords(3004));
+		Assert.assertEquals("five thousand and twenty-six", ntn.converterToWords(5026));
+		Assert.assertEquals("seven thousand and one hundred and twelve", ntn.converterToWords(7112));
+	}
+	
+	@Test
+	public void return_five_digit_number_in_words() {
+		Assert.assertEquals("fourty thousand", ntn.converterToWords(40000));
+		Assert.assertEquals("eighteen thousand", ntn.converterToWords(18000));
+		Assert.assertEquals("eighty-seven thousand and six hundred and fifty-four", ntn.converterToWords(87654));
+	}
 
 }
