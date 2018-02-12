c3728daadf3f6a88be9489395c8eae1b62cf5476
anastasia <anastasia.prokofyeva@gmail.com>
2018-02-11 20:15:16 -0500

    update

---
+++ m1-capstone/src/main/java/com/techelevator/PrintCsv.java
@@ -0,0 +1,31 @@
+package com.techelevator;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PrintCsv {
+	
+	public void printFile() throws FileNotFoundException, IOException {
+		
+		String path = "";
+		
+		File inventory = new File(path);
+	
+		try(BufferedReader reader = new BufferedReader(new FileReader(inventory))){
+			String line = reader.readLine();
+			String printOut = null;
+			//String delimiter = "|";
+			while(line != null) {
+				for (int i = 0; i < line.length(); i++) {
+					printOut += (line + "\n");
+				}
+			}
+		System.out.println(printOut);
+		
+		}
+	}
+ 
+}
